10b7ffd8fb31c2a204f5e89b738f4fd03ee100f3
Marcel Macukic <147931125+Mmacukic@users.noreply.github.com>
2024-10-26 18:50:43 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const client = new Client({
 	user: 'web2auth_pw37_user',
 	password: 'YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm',
-	host: 'postgresql://web2auth_pw37_user:YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm@dpg-csehnebtq21c738c6fc0-a.frankfurt-postgres.render.com/web2auth_pw37',
+	host: 'postgresql://web2auth_pw37_user:YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm@dpg-csehnebtq21c738c6fc0-a/web2auth_pw37',
 	port: '5432',
 	database: 'web2auth_pw37',
 });
