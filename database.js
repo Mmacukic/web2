34a78b3cf1afd7083c9635bfd856d1819bd91c6b
Marcel Macukic <147931125+Mmacukic@users.noreply.github.com>
2024-10-26 23:15:14 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const client = new Client({
 	user: 'web2auth_pw37_user',
 	password: 'YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm',
-	hostName: 'dpg-csehnebtq21c738c6fc0-a',
+	database: 'web2auth_pw37',
+	host: 'dpg-csehnebtq21c738c6fc0-a',
 	port: '5432',
-	database: 'web2auth_pw37'
 });
 client
 	.connect()
