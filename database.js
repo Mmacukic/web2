4a75b6e5ab6f7621cb7c07631f30165a2ef47ae6
Marcel Macukic <147931125+Mmacukic@users.noreply.github.com>
2024-10-26 19:15:05 +0200

    Update database.js

--- database.js
+++ database.js
@@ -5,8 +5,7 @@ const client = new Client({
 	password: 'YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm',
 	hostName: 'dpg-csehnebtq21c738c6fc0-a',
 	port: '5432',
-	database: 'web2auth_pw37',
-	internalDatabaseUrl: "postgresql://web2auth_pw37_user:YmSMJWYCxBNB7H2mEjnAlEEMDn3N9uXm@dpg-csehnebtq21c738c6fc0-a/web2auth_pw37",
+	database: 'web2auth_pw37'
 });
 client
 	.connect()
